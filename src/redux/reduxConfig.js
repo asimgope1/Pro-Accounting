245d34c76546943445ab99db0e0c235ed0c520d8
asimgope1 <asimgope53@gmail.com>
2025-01-29 11:50:10 +0530

    first commit pro-Accounting

---
+++ src/redux/reduxConfig.js
@@ -0,0 +1,16 @@
+import {configureStore,applyMiddleware,compose} from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
+
+const middleware = [
+    thunk
+]
+
+export default configure = (initialState = {}) => {
+    return configureStore(
+        rootReducer,
+        initialState,
+        compose(applyMiddleware(...middleware)))
+}
+
+
